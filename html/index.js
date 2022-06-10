2d6be0f8da501cc3a8d2af8dc4c9c3914138e49c
Plactrix <48103197+Plactrix@users.noreply.github.com>
2022-06-09 20:45:52 -0700

    Update index.js
    Removed even more unnecessary spaces

--- html/index.js
+++ html/index.js
@@ -15,12 +15,8 @@ $(function () {
         }
     }
 
-
-
     display(false)
     displayAlready(false)
-
-
 
     // if the person uses the escape key, it will exit the resource
     document.onkeyup = function (data) {
@@ -36,29 +32,8 @@ $(function () {
         }
     };
 
-
-
-
-
-
-
-
-
     window.addEventListener('message', function(event) {
         var item = event.data;
-
-//        document.getElementById("slot1Name").innerHTML = item.slot1Name;
-//        document.getElementById("slot2Name").innerHTML = item.slot2Name;
-//        document.getElementById("slot3Name").innerHTML = item.slot3Name;
-
-//        document.getElementById("genderSlot1").innerHTML = item.slot1Gender;
-//        document.getElementById("genderSlot2").innerHTML = item.slot2Gender;
-//        document.getElementById("genderSlot3").innerHTML = item.slot3Gender;
-
-//        document.getElementById("partySlot1").innerHTML = item.slot1Age;
-//        document.getElementById("partySlot2").innerHTML = item.slot2Age;
-//        document.getElementById("partySlot3").innerHTML = item.slot3Age;
-
 
         if (item.type === "ui") {
             if (item.status == true) {
@@ -76,44 +51,4 @@ $(function () {
             }
         }
     })
-   
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//    document.onkeyup = function (data) {
-//        if (data.which == 27) {
-//            $.post('http://voting/exit', JSON.stringify({})); // Wenn man ESC triggert er Callback exit in der Main.lua Client
-//            return
-//        }
-//    };
-//    $("#close").click(function () {
-//        $.post('http://voting/exit', JSON.stringify({})); //exit ist der Callback in der Client Main.lua - Sobald der Button mit der ID Close gedrückt wird passiert das was in der Callback steht - In dem fall schließt er das
-//        return
-//    })
-
-//    $("#alreadyCheckOkay").click(function () {
-//        $.post('http://voting/exit', JSON.stringify({})); //exit ist der Callback in der Client Main.lua - Sobald der Button mit der ID Close gedrückt wird passiert das was in der Callback steht - In dem fall schließt er das
-//        return
-//    })
-
-//    $("#thxVote").click(function () {
-//        $("#contentSuccess").hide("100");
-//        $.post('http://voting/exit', JSON.stringify({})); //exit ist der Callback in der Client Main.lua - Sobald der Button mit der ID Close gedrückt wird passiert das was in der Callback steht - In dem fall schließt er das
-//        return
-//    })
-
-   
-    
 })
